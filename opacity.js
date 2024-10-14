05083e6232bc76aa5142e6928eb641a5e4c081c1
xcatzelaravel <xcatzelaravel@gmail.com>
2024-10-14 15:48:35 -0500

    Update opacity.js

--- opacity.js
+++ opacity.js
@@ -8,7 +8,7 @@
         var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
         var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
         
-        if(days > 0) { // Verifica si la fecha de vencimiento ya ha pasado
+        if(days > 0) {
           var days_late = days_deadline - days;
           var opacity = (days_late * 100 / days_deadline) / 100;
             opacity = (opacity < 0) ? 0 : opacity;
